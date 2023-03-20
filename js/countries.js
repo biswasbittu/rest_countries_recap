2abe9e321e84826271e39927ffff1fc3ac50d14b
biswasbittu <biswasbittu7415@yahoo.com>
2023-03-20 13:13:56 +0530

    destraing done

--- js/countries.js
+++ js/countries.js
@@ -5,21 +5,46 @@ const loadCountries=()=>{
 }
 
 const displayCountries=countries=>{
-//    console.log(countries);
+   console.log(countries);
    const countriesHtml=countries.map(country=>gteCountryHTML(country))
-        // console.log(countriesHtml[0])
+        // console.log(country)
         const container=document.getElementById('countries');
         container.innerHTML=countriesHtml.join(" ");
 
 
 }
 
-const gteCountryHTML=country=>{
-    console.log(country.flags )
-    return `
-    <div class="country">
-    <h2>${country.name.common}</h2>
-    <img src="${country.flags.png}"/>
-    </div>`
-}
+// const gteCountryHTML=country=>{
+//     console.log(country )
+//     // console.log(country.flags )
+//     const {name,flags}=country
+//     return `
+//     <div class="country">
+//     <h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}"/>
+//     </div>`
+// }
+
+// const gteCountryHTML=country=>{
+//     // console.log(country.flags )
+//     const {name,flags}=country
+//     return `
+//     <div class="country">
+//     <h2>${name.common}</h2>
+//     <img src="${flags.png}"/>
+//     </div>`
+// }
+
+const gteCountryHTML=({name,flags,area,subregion})=>{
+        // console.log(country.flags )
+    //    const {name,flags}=country;
+
+        return `
+        <div class="country">
+        <h2>${name.common}</h2>
+        <img src="${flags.png}"/>
+        <p>Area:${area}</p>
+        <p>Region:${subregion}</p>
+        </div>`
+    }
 loadCountries();
